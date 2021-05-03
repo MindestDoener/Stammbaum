59c9e378fffeafdd8bf9e5e1cd8746b798e6d081
EagleEars <60156681+elias-wagner@users.noreply.github.com>
2021-05-03 15:21:01 +0200

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditorComponent } from './editor/editor.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: LandingPageComponent
+  },
+  {
+    path: 'editor',
+    component: EditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
