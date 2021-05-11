16e0343e3220c819e97d46cd92ee6f73c2333a46
Justus Ernst <justernst.je@gmail.com>
2021-05-11 13:20:04 +0200

    Refactor: Adding GraphManager

---
+++ src/app/Components/editor/GraphManager.ts
@@ -0,0 +1,60 @@
+import {Edge, Node} from '@swimlane/ngx-graph';
+import {Person} from '../../shared/types';
+
+export class GraphManager {
+  nodes: Node[] = [];
+  edges: Edge[] = [];
+
+  private static buildLabel(person: Person): string {
+    return person.firstName + ' ' + person.lastName;
+  }
+
+  private static createNode(person: Person): Node {
+    return {
+      id: person.id.toString(),
+      label: GraphManager.buildLabel(person),
+      dimension: {width: 200, height: 40},
+      data: {customColor: person.gender.color},
+    };
+  }
+
+  public updateNode(person: Person): void {
+    const oldNode = this.findNodeById(person.id);
+    const newNode = GraphManager.createNode(person);
+    person.node = newNode;
+    if (oldNode) {
+      const index = this.nodes.indexOf(oldNode);
+      if (index > -1) {
+        this.nodes[index] = newNode;
+        this.updateTree();
+      }
+    }
+  }
+
+  public createNewNode(person: Person): void {
+    const node = GraphManager.createNode(person);
+    person.node = node;
+    this.nodes.push(node);
+    this.updateTree();
+  }
+
+  public removeNode(person: Person): void {
+    if (person.node) {
+      const index = this.nodes.indexOf(person.node, 0);
+      if (index > -1) {
+        this.nodes.splice(index, 1);
+        this.updateTree();
+      }
+    }
+  }
+
+  findNodeById(id: number): Node | undefined {
+    return this.nodes.find((node) => +node.id === id);
+  }
+
+  updateTree(): void {
+    this.nodes = [...this.nodes];
+    this.edges = [...this.edges];
+  }
+
+}
--- src/app/Components/editor/editor.component.ts
+++ src/app/Components/editor/editor.component.ts
@@ -1,20 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {StammbaumServiceService} from '../../shared/stammbaum-service.service';
-import {convertDate, CreatePersonRequest, Gender, Person, Stammbaum} from '../../shared/types';
+import {CreatePersonRequest, Gender, Person, Stammbaum} from '../../shared/types';
 import {FormControl, FormGroup} from '@angular/forms';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {ContextMenuContentComponent} from './context-menu-content/context-menu-content.component';
-import { ActivatedRoute } from '@angular/router';
-import {Edge, Node} from '@swimlane/ngx-graph';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Node} from '@swimlane/ngx-graph';
+import {GraphManager} from './GraphManager';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit{
+export class EditorComponent {
 
-  constructor(private stammbaumService: StammbaumServiceService, private modalService: NgbModal, private route: ActivatedRoute) {
+  constructor(private stammbaumService: StammbaumServiceService, private modalService: NgbModal,
+              private route: ActivatedRoute, private router: Router) {
+    const params = this.route.snapshot.paramMap;
+    try {
+      this.stammbaum = this.stammbaumService.getSingleTree(params.get('id'));
+    } catch (e) {
+      router.navigate(['/home']); // redirect home when invalid tree id
+    }
   }
 
   addPersonForm = new FormGroup({
@@ -27,23 +35,9 @@ export class EditorComponent implements OnInit{
 
   stammbaum!: Stammbaum;
 
-  nodes: Node[] = [];
-  links: Edge[] = [];
+  graphManager: GraphManager = new GraphManager();
 
   genders = ['Male', 'Female', 'Diverse'];
-
-  private static getValue(person: Person): string {
-    if (person.deathDate) {
-      return person.firstName + ' ' + person.lastName +
-        '\n * ' + convertDate(person.birthDate) + ' - † ' + convertDate(person.deathDate);
-    }
-    return person.firstName + ' ' + person.lastName + '\n * ' + convertDate(person.birthDate);
-  }
-
-  ngOnInit(): void {
-    const params = this.route.snapshot.paramMap;
-    this.stammbaum = this.stammbaumService.getSingleTree(params.get('id'));
-  }
 
   dblClickEvent = (node: Node) => {
     const person = this.stammbaumService.getPersonById(+node.id, this.stammbaum.id);
@@ -57,28 +51,25 @@ export class EditorComponent implements OnInit{
     personRequest.gender = Gender.getById(this.addPersonForm.value.gender);
     const person = this.stammbaumService.addPerson(personRequest, this.stammbaum.id);
     this.addPersonForm.reset();
-    const node: Node = {
-      id: person.id.toString(),
-      label: EditorComponent.getValue(person),
-      dimension: {width: 200, height: 40},
-      data: {customColor: person.gender.color},
-    };
-    person.node = node;
-    this.nodes.push(node);
-    this.nodes = [...this.nodes];
+    this.graphManager.createNewNode(person);
   }
 
   onOpenContextMenu(person: Person): void {
     const modalRef = this.modalService.open(ContextMenuContentComponent, {size: 'lg'});
     modalRef.componentInstance.person = person;
-    modalRef.componentInstance.deletePerson.subscribe((personToDelete: Person) => {
-      this.stammbaumService.deletePerson(personToDelete, this.stammbaum.id);
-      modalRef.close();
-    });
+    modalRef.componentInstance.stammbaum = this.stammbaum;
+    modalRef.componentInstance.deletePerson.subscribe((personToDelete: Person) => this.deletePersonEvent(personToDelete, modalRef));
     modalRef.componentInstance.updatePerson.subscribe(this.updatePersonEvent);
   }
 
   updatePersonEvent = (personToUpdate: Person) => {
+    this.graphManager.updateNode(personToUpdate);
     this.stammbaumService.updatePerson(personToUpdate, this.stammbaum.id);
+  }
+
+  deletePersonEvent = (personToDelete: Person, modalRef: NgbModalRef) => {
+    this.stammbaumService.deletePerson(personToDelete, this.stammbaum.id);
+    this.graphManager.removeNode(personToDelete);
+    modalRef.close();
   }
 }
