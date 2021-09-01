cdd0ec09313cd26744876581f822f6886ef6fc16
Justus Ernst <justernst.je@gmail.com>
2021-09-01 13:24:12 +0200

    adds json import

--- src/app/Components/editor/export-menu/export-menu.component.spec.ts
+++ src/app/Components/editor/export-menu/export-menu.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExportMenuComponent } from './export-menu.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('ExportMenuComponent', () => {
   let component: ExportMenuComponent;
@@ -8,7 +11,10 @@ describe('ExportMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExportMenuComponent ]
+      declarations: [ ExportMenuComponent ],
+      providers: [
+        { provide: NgbActiveModal, useValue: NgbActiveModal },
+      ]
     })
     .compileComponents();
   });
--- src/app/shared/family-tree.service.ts
+++ src/app/shared/family-tree.service.ts
@@ -9,70 +9,39 @@ import { getNow, getToday, Time } from './types/time';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
-import { CreateFamilyTreeModel, FamilyTreeModel } from './api/models/familyTreeModel';
+import { Config, CreateFamilyTreeModel, FamilyTreeModel } from './api/models/familyTreeModel';
 import { AuthService } from './auth.service';
 import { TreeApiService } from './api/tree-api.service';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class FamilyTreeService {
-    constructor(private auth: AuthService, private treeApi: TreeApiService) {}
+  familyTreeMap: Map<string, FamilyTree> = TestData.testList;
 
-    familyTreeMap: Map<string, FamilyTree> = TestData.testList;
-
-    private static sortByLastChanged(list: FamilyTree[]): FamilyTree[] {
-        return list.sort((a, b) => {
-            if (a && b) {
-                if (a.lastChanged.date.before(b.lastChanged.date)) {
-                    return 1;
-                }
-                if (a.lastChanged.date.after(b.lastChanged.date)) {
-                    return -1;
-                }
-                if (a.lastChanged.time.before(b.lastChanged.time)) {
-                    return 1;
-                }
-                if (a.lastChanged.time.after(b.lastChanged.time)) {
-                    return -1;
-                }
-            }
-            return 0;
-        });
-    }
-
-    private static sortByNameAlphabetic(list: FamilyTree[]): FamilyTree[] {
-        return list.sort((a, b) => {
-            return a.name.localeCompare(b.name);
-        });
-    }
-
-    private static sortByNumberOfPersons(list: FamilyTree[]): FamilyTree[] {
-        return list.sort((a, b) => {
-            return b.persons.size - a.persons.size;
-        });
-    }
+  constructor(private auth: AuthService, private treeApi: TreeApiService) {
+  }
 
   public static mapModelToObject(tree: FamilyTreeModel): FamilyTree {
-      const lastChanged = tree.config.lastChanged;
-      const personMap: Map<number, Person> = new Map(tree.config.persons);
-      return {
-          ...tree.config,
-          id: tree.id.toString(),
-          persons: personMap,
-          lastChanged: {
-              date: new NgbDate(
-                  lastChanged.date.year,
-                  lastChanged.date.month,
-                  lastChanged.date.day
-              ),
-              time: new Time(
-                  lastChanged.time.hours,
-                  lastChanged.time.minutes,
-                  lastChanged.time.seconds
-              ),
-          },
-      };
+    const lastChanged = tree.config.lastChanged;
+    const personMap: Map<number, Person> = new Map(tree.config.persons);
+    return {
+      ...tree.config,
+      id: tree.id.toString(),
+      persons: personMap,
+      lastChanged: {
+        date: new NgbDate(
+          lastChanged.date.year,
+          lastChanged.date.month,
+          lastChanged.date.day,
+        ),
+        time: new Time(
+          lastChanged.time.hours,
+          lastChanged.time.minutes,
+          lastChanged.time.seconds,
+        ),
+      },
+    };
   }
 
   static tree2Blob(tree: FamilyTree): Blob {
@@ -80,94 +49,165 @@ export class FamilyTreeService {
     return new Blob([JSON.stringify(parsableTree)], { type: 'application/json' });
   }
 
-    createEmptyFamilyTree(name: string): Observable<FamilyTreeModel> {
+  private static sortByLastChanged(list: FamilyTree[]): FamilyTree[] {
+    return list.sort((a, b) => {
+      if (a && b) {
+        if (a.lastChanged.date.before(b.lastChanged.date)) {
+          return 1;
+        }
+        if (a.lastChanged.date.after(b.lastChanged.date)) {
+          return -1;
+        }
+        if (a.lastChanged.time.before(b.lastChanged.time)) {
+          return 1;
+        }
+        if (a.lastChanged.time.after(b.lastChanged.time)) {
+          return -1;
+        }
+      }
+      return 0;
+    });
+  }
+
+  private static sortByNameAlphabetic(list: FamilyTree[]): FamilyTree[] {
+    return list.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+  }
+
+  private static sortByNumberOfPersons(list: FamilyTree[]): FamilyTree[] {
+    return list.sort((a, b) => {
+      return b.persons.size - a.persons.size;
+    });
+  }
+
+  createEmptyFamilyTree(name: string): Observable<FamilyTreeModel> {
+    const familyTree: CreateFamilyTreeModel = {
+      config: {
+        name,
+        persons: Array.from(new Map<number, Person>()),
+        lastChanged: { date: getToday(), time: { hours: getNow().hours, minutes: getNow().minutes, seconds: getNow().seconds || 0 } },
+      },
+      username: this.auth.getUsername(),
+    };
+    return this.treeApi.createTree(familyTree);
+  }
+
+  importTree(file: File): Observable<FamilyTreeModel> {
+    return new Observable<FamilyTreeModel>(resultObserver => {
+      const readFile = new Observable<Config>(observer => {
+        const reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+        reader.onload = () => {
+          observer.next(JSON.parse(reader.result as string));
+        };
+        reader.onerror = () => {
+          observer.error(reader.error);
+        };
+      });
+
+      readFile.subscribe(data => {
         const familyTree: CreateFamilyTreeModel = {
           config: {
-            name,
-            persons: Array.from(new Map<number, Person>()),
-            lastChanged: { date: getToday(), time: { hours: getNow().hours, minutes: getNow().minutes, seconds: getNow().seconds || 0 } },
-            },
-          username: this.auth.getUsername()
+            name: data.name,
+            persons: data.persons,
+            lastChanged: data.lastChanged,
+          },
+          username: this.auth.getUsername(),
         };
-        return this.treeApi.createTree(familyTree);
-    }
+        this.treeApi.createTree(familyTree).subscribe(tree => {
+          resultObserver.next(tree);
+        });
+      });
+    });
+  }
 
-    deleteFamilyTree(id: string): Observable<any> {
-        return this.treeApi.deleteTree(id);
-    }
+  deleteFamilyTree(id: string): Observable<any> {
+    return this.treeApi.deleteTree(id);
+  }
 
-    addPerson(familyTree: FamilyTree, personRequest: CreatePersonRequest): Person {
-        const person = {
-            id: this.makeUUID(familyTree),
-            ...personRequest,
-        };
-        familyTree.persons.set(person.id, person);
-        this.updateLastChanged(familyTree).subscribe();
-        return person;
-    }
+  addPerson(familyTree: FamilyTree, personRequest: CreatePersonRequest): Person {
+    const person = {
+      id: this.makeUUID(familyTree),
+      ...personRequest,
+    };
+    familyTree.persons.set(person.id, person);
+    this.updateLastChanged(familyTree).subscribe();
+    return person;
+  }
 
-    // Takes Person as input and changes data of person in map with same id to data of person in parameter
-    updatePerson(familyTree: FamilyTree, person: Person): void {
-        familyTree.persons.set(person.id, person);
-        this.updateLastChanged(familyTree).subscribe();
-    }
+  // Takes Person as input and changes data of person in map with same id to data of person in parameter
+  updatePerson(familyTree: FamilyTree, person: Person): void {
+    familyTree.persons.set(person.id, person);
+    this.updateLastChanged(familyTree).subscribe();
+  }
 
-    deletePerson(familyTree: FamilyTree, person: Person): void {
-        familyTree.persons.delete(person.id);
-        this.updateLastChanged(familyTree).subscribe();
-    }
+  deletePerson(familyTree: FamilyTree, person: Person): void {
+    familyTree.persons.delete(person.id);
 
-    getPersonById(familyTree: FamilyTree, id: number): Person | undefined {
-        return familyTree.persons.get(id);
-    }
+    familyTree.persons.forEach(
+      otherPerson =>
+        // @ts-ignore
+        otherPerson.children = otherPerson.children.filter(
+          childId => childId !== person.id,
+        ),
+    );
+    this.updateLastChanged(familyTree).subscribe();
+  }
 
-    getTreeMap(): Map<string, FamilyTree> {
-        return this.familyTreeMap;
-    }
+  getPersonById(familyTree: FamilyTree, id: number): Person | undefined {
+    return familyTree.persons.get(id);
+  }
 
-    getTreeList(): Observable<FamilyTree[]> {
-        return this.treeApi.getTreesForUser();
-    }
+  getTreeMap(): Map<string, FamilyTree> {
+    return this.familyTreeMap;
+  }
 
-    getTreeListSorted(sortMode: SortMode): Observable<FamilyTree[]> {
-        return this.getTreeList().pipe(
-            map((list) => {
-                switch (sortMode) {
-                    case SortMode.lastChanged:
-                        return FamilyTreeService.sortByLastChanged(list);
-                    case SortMode.alphabetic:
-                        return FamilyTreeService.sortByNameAlphabetic(list);
-                    case SortMode.persons:
-                        return FamilyTreeService.sortByNumberOfPersons(list);
-                }
-            })
-        );
-    }
+  getTreeList(): Observable<FamilyTree[]> {
+    return this.treeApi.getTreesForUser();
+  }
 
-    getSingleTree(id: string | null): Observable<FamilyTree> {
-      if (!id) { throw new Error("id was null"); }
-      return this.treeApi.getSingleTree(id);
-    }
-
-    updateLastChanged(familyTree: FamilyTree): Observable<any> {
-        familyTree.lastChanged = { date: getToday(), time: getNow() };
-        const updateFamilyTree: CreateFamilyTreeModel = {
-            config: {
-             name: familyTree.name,
-              persons: Array.from(familyTree.persons),
-              lastChanged: { date: familyTree.lastChanged.date, time: familyTree.lastChanged.time },
-              },
-            username: this.auth.getUsername()
-          };
-        return this.treeApi.updateTree(familyTree,updateFamilyTree);
-    }
-
-    private makeUUID(tree: FamilyTree): number {
-        const num = makeUUID();
-        // check for duplicate ids
-        if (tree.persons.has(num)) {
-                return this.makeUUID(tree);
+  getTreeListSorted(sortMode: SortMode): Observable<FamilyTree[]> {
+    return this.getTreeList().pipe(
+      map((list) => {
+        switch (sortMode) {
+          case SortMode.lastChanged:
+            return FamilyTreeService.sortByLastChanged(list);
+          case SortMode.alphabetic:
+            return FamilyTreeService.sortByNameAlphabetic(list);
+          case SortMode.persons:
+            return FamilyTreeService.sortByNumberOfPersons(list);
         }
-        return num;
+      }),
+    );
+  }
+
+  getSingleTree(id: string | null): Observable<FamilyTree> {
+    if (!id) {
+      throw new Error('id was null');
+    }
+    return this.treeApi.getSingleTree(id);
+  }
+
+  updateLastChanged(familyTree: FamilyTree): Observable<any> {
+    familyTree.lastChanged = { date: getToday(), time: getNow() };
+    const updateFamilyTree: CreateFamilyTreeModel = {
+      config: {
+        name: familyTree.name,
+        persons: Array.from(familyTree.persons),
+        lastChanged: { date: familyTree.lastChanged.date, time: familyTree.lastChanged.time },
+      },
+      username: this.auth.getUsername(),
+    };
+    return this.treeApi.updateTree(familyTree, updateFamilyTree);
+  }
+
+  private makeUUID(tree: FamilyTree): number {
+    const num = makeUUID();
+    // check for duplicate ids
+    if (tree.persons.has(num)) {
+      return this.makeUUID(tree);
+    }
+    return num;
   }
 }
