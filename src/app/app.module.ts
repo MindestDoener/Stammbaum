40acb3356e084d8ad22e86feefc2c8c9fa7f304e
EagleEars <60156681+elias-wagner@users.noreply.github.com>
2022-02-20 21:23:36 +0100

    translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { TreeListComponent } from './Components/tree-list/tree-list.component';
 import { TreeListItemComponent } from './Components/tree-list/tree-list-item/tree-list-item.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginPageComponent } from './Components/login-page/login-page.component';
 import { ExportMenuComponent } from './Components/editor/export-menu/export-menu.component';
 import { ChildSelectionComponent } from './Components/editor/child-selection/child-selection.component';
@@ -21,6 +21,10 @@ import { NodeGraphicComponent } from './Components/node-graphic/node-graphic.com
 import { WaveGraphicComponent } from './Components/wave-graphic/wave-graphic.component';
 import { AboutComponent } from './Components/about-page/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+// import ngx-translate
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 
 @NgModule({
@@ -47,9 +51,21 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     NgbModule,
     HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
+}
+
+// required for AOT (ahead of time) compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
 }
